1064821c0f3a0234bc199ae6227aeb124388751a
SDWLAD <sidorchukvladislav0@gmail.com>
2024-08-17 20:50:37 +0300

    First quad

--- main.py
+++ main.py
@@ -1,70 +1,69 @@
-import ctypes  
-import pyglet
-
-pyglet.options["shadow_window"] = False
-pyglet.options["debug_gl"] = False
-
-import pyglet.gl as gl
-
-vertex_positions = [  
-	-0.5, 0.5, 1.0,
-	-0.5, -0.5, 1.0,
-	0.5, -0.5, 1.0, 
-]
-
-indices = [
-	0, 1, 2,  
-]
+from settings import *
+import moderngl as mgl
+import pygame as pg
+import sys
+from shader_program import ShaderProgram
+from scene import Scene
+from player import Player
 
 
-class Window(pyglet.window.Window):
-	def __init__(self, **args):
-		super().__init__(**args)
-		self.vao = gl.GLuint(0)
-		gl.glGenVertexArrays(1, ctypes.byref(self.vao))
-		gl.glBindVertexArray(self.vao)
-		
-		self.vbo = gl.GLuint(0)
-		gl.glGenBuffers(1, ctypes.byref(self.vbo))
-		gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vbo)
-		gl.glBufferData(
-			gl.GL_ARRAY_BUFFER,
-			ctypes.sizeof(gl.GLfloat * len(vertex_positions)),
-			(gl.GLfloat * len(vertex_positions))(*vertex_positions),
-			gl.GL_STATIC_DRAW,
-		)
-		gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, 0)
-		gl.glEnableVertexAttribArray(0)
-		
-		self.ibo = gl.GLuint(0)
-		gl.glGenBuffers(1, self.ibo)
-		gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.ibo)
-
-		gl.glBufferData(
-			gl.GL_ELEMENT_ARRAY_BUFFER,
-			ctypes.sizeof(gl.GLuint * len(indices)),
-			(gl.GLuint * len(indices))(*indices),
-			gl.GL_STATIC_DRAW,
-		)
-
-	def on_draw(self):
-		gl.glClearColor(0,0,0,1)
-		self.clear()
-		gl.glDrawElements(gl.GL_TRIANGLES, len(indices), gl.GL_UNSIGNED_INT, None)  
-
-	def on_resize(self, width, height):
-		print(f"Resize {width} * {height}")
-		gl.glViewport(0, 0, width, height)  
-
-
-class Game:
+class VoxelEngine:
     def __init__(self):
-        self.config = gl.Config(double_buffer=True, major_version=3, minor_version=3)
-        self.window = Window(width=800, height=600, config=self.config, caption="PygletMinecraft")
+        pg.init()
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE, 24)
+
+        pg.display.set_mode(WIN_RES, flags=pg.OPENGL | pg.DOUBLEBUF)
+        self.ctx = mgl.create_context()
+
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
+        self.ctx.gc_mode = 'auto'
+
+        self.clock = pg.time.Clock()
+        self.delta_time = 0
+        self.time = 0
+
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+
+        self.is_running = True
+        self.on_init()
+
+    def on_init(self):
+        self.player = Player(self)
+        self.shader_program = ShaderProgram(self)
+        self.scene = Scene(self)
+
+    def update(self):
+        self.player.update()
+        self.shader_program.update()
+        self.scene.update()
+
+        self.delta_time = self.clock.tick()
+        self.time = pg.time.get_ticks() * 0.001
+        pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
+
+    def render(self):
+        self.ctx.clear(color=BG_COLOR)
+        self.scene.render()
+        pg.display.flip()
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.is_running = False
 
     def run(self):
-        pyglet.app.run()
+        while self.is_running:
+            self.handle_events()
+            self.update()
+            self.render()
+        pg.quit()
+        sys.exit()
 
-if __name__ == "__main__":
-	game = Game()
-	game.run()
+
+if __name__ == '__main__':
+    app = VoxelEngine()
+    app.run()
