733f2fe53a3735f18062538f416e8cfee0b4811e
SDWLAD <sidorchukvladislav0@gmail.com>
2024-09-10 22:12:33 +0300

    first step for the data packing.

--- mathematics/chunk_mesh_builder.py
+++ mathematics/chunk_mesh_builder.py
@@ -21,8 +21,25 @@ def get_ao(local_pos, chunk_blocks, chunks, plane):
 
 
 @njit(fastmath=True)
-def to_uint8(x, y, z, voxel_id, face_id, ao_id):
-    return uint8(x), uint8(y), uint8(z), uint8(voxel_id), uint8(face_id), uint8(ao_id)
+def pack_data(x, y, z, voxel_id, face_id, ao_id):
+    a, b, c, d, e, f, g = x, y, z, voxel_id, face_id, ao_id, 1
+
+    b_bit, c_bit, d_bit, e_bit, f_bit, g_bit = 6, 6, 8, 3, 2, 1
+    fg_bit = f_bit + g_bit
+    efg_bit = e_bit + fg_bit
+    defg_bit = d_bit + efg_bit
+    cdefg_bit = c_bit + defg_bit
+    bcdefg_bit = b_bit + cdefg_bit
+
+    packed_data = (
+        a << bcdefg_bit |
+        b << cdefg_bit |
+        c << defg_bit |
+        d << efg_bit |
+        e << fg_bit |
+        f << g_bit | g
+    )
+    return packed_data
 
 @njit(fastmath=True)
 def is_void(block_pos, chunk_blocks, chunks):
@@ -40,13 +57,13 @@ def is_void(block_pos, chunk_blocks, chunks):
 @njit(fastmath=True)
 def add_data(vertex_data, index, *vertices):
     for vertex in vertices:
-        vertex_data[index:index+len(vertex)] = vertex
-        index += len(vertex)
+        vertex_data[index] = vertex
+        index += 1
     return index
 
 @njit(fastmath=True)
 def build_chunk_mesh(chunk_blocks, format_size, chunks):
-    vertex_data = np.empty((CHUNK_WIDTH*CHUNK_WIDTH*CHUNK_HEIGHT * 18 * format_size,), dtype=np.uint8)
+    vertex_data = np.empty((CHUNK_WIDTH*CHUNK_WIDTH*CHUNK_HEIGHT * 18 * format_size,), dtype=np.uint32)
     index = 0
 
     for x in range(CHUNK_WIDTH):
@@ -61,55 +78,55 @@ def build_chunk_mesh(chunk_blocks, format_size, chunks):
                 if is_void((x, y + 1, z), chunk_blocks, chunks):
                     ao = get_ao((x, y + 1, z), chunk_blocks, chunks, plane='Y')
 
-                    v0 = to_uint8(x, y + 1, z, block_id, 0, ao[0])
-                    v1 = to_uint8(x + 1, y + 1, z, block_id, 0, ao[1])
-                    v2 = to_uint8(x + 1, y + 1, z + 1, block_id, 0, ao[2])
-                    v3 = to_uint8(x, y + 1, z + 1, block_id, 0, ao[3])
+                    v0 = pack_data(x, y + 1, z, block_id, 0, ao[0])
+                    v1 = pack_data(x + 1, y + 1, z, block_id, 0, ao[1])
+                    v2 = pack_data(x + 1, y + 1, z + 1, block_id, 0, ao[2])
+                    v3 = pack_data(x, y + 1, z + 1, block_id, 0, ao[3])
                     index = add_data(vertex_data, index, v0, v3, v2, v0, v2, v1)
 
                 if is_void((x, y - 1, z), chunk_blocks, chunks) and y != 0:
                     ao = get_ao((x, y - 1, z), chunk_blocks, chunks, plane='Y')
 
-                    v0 = to_uint8(x, y, z, block_id, 1, ao[0])
-                    v1 = to_uint8(x + 1, y, z, block_id, 1, ao[1])
-                    v2 = to_uint8(x + 1, y, z + 1, block_id, 1, ao[2])
-                    v3 = to_uint8(x, y, z + 1, block_id, 1, ao[3])
+                    v0 = pack_data(x, y, z, block_id, 1, ao[0])
+                    v1 = pack_data(x + 1, y, z, block_id, 1, ao[1])
+                    v2 = pack_data(x + 1, y, z + 1, block_id, 1, ao[2])
+                    v3 = pack_data(x, y, z + 1, block_id, 1, ao[3])
                     index = add_data(vertex_data, index, v0, v2, v3, v0, v1, v2)
 
                 if is_void((x + 1, y, z), chunk_blocks, chunks):
                     ao = get_ao((x + 1, y, z), chunk_blocks, chunks, plane='X')
 
-                    v0 = to_uint8(x + 1, y, z, block_id, 2, ao[0])
-                    v1 = to_uint8(x + 1, y + 1, z, block_id, 2, ao[1])
-                    v2 = to_uint8(x + 1, y + 1, z + 1, block_id, 2, ao[2])
-                    v3 = to_uint8(x + 1, y, z + 1, block_id, 2, ao[3])
+                    v0 = pack_data(x + 1, y, z, block_id, 2, ao[0])
+                    v1 = pack_data(x + 1, y + 1, z, block_id, 2, ao[1])
+                    v2 = pack_data(x + 1, y + 1, z + 1, block_id, 2, ao[2])
+                    v3 = pack_data(x + 1, y, z + 1, block_id, 2, ao[3])
                     index = add_data(vertex_data, index, v0, v1, v2, v0, v2, v3)
 
                 if is_void((x - 1, y, z), chunk_blocks, chunks):
                     ao = get_ao((x - 1, y, z), chunk_blocks, chunks, plane='X')
 
-                    v0 = to_uint8(x, y, z, block_id, 3, ao[0])
-                    v1 = to_uint8(x, y + 1, z, block_id, 3, ao[1])
-                    v2 = to_uint8(x, y + 1, z + 1, block_id, 3, ao[2])
-                    v3 = to_uint8(x, y, z + 1, block_id, 3, ao[3])
+                    v0 = pack_data(x, y, z, block_id, 3, ao[0])
+                    v1 = pack_data(x, y + 1, z, block_id, 3, ao[1])
+                    v2 = pack_data(x, y + 1, z + 1, block_id, 3, ao[2])
+                    v3 = pack_data(x, y, z + 1, block_id, 3, ao[3])
                     index = add_data(vertex_data, index, v0, v2, v1, v0, v3, v2)
 
                 if is_void((x, y, z - 1), chunk_blocks, chunks):
                     ao = get_ao((x, y, z - 1), chunk_blocks, chunks, plane='Z')
 
-                    v0 = to_uint8(x, y, z, block_id, 4, ao[0])
-                    v1 = to_uint8(x, y + 1, z, block_id, 4, ao[1])
-                    v2 = to_uint8(x + 1, y + 1, z, block_id, 4, ao[2])
-                    v3 = to_uint8(x + 1, y, z, block_id, 4, ao[3])
+                    v0 = pack_data(x, y, z, block_id, 4, ao[0])
+                    v1 = pack_data(x, y + 1, z, block_id, 4, ao[1])
+                    v2 = pack_data(x + 1, y + 1, z, block_id, 4, ao[2])
+                    v3 = pack_data(x + 1, y, z, block_id, 4, ao[3])
                     index = add_data(vertex_data, index, v0, v1, v2, v0, v2, v3)
 
                 if is_void((x, y, z + 1), chunk_blocks, chunks):
                     ao = get_ao((x, y, z + 1), chunk_blocks, chunks, plane='Z')
 
-                    v0 = to_uint8(x, y, z + 1, block_id, 5, ao[0])
-                    v1 = to_uint8(x, y + 1, z + 1, block_id, 5, ao[1])
-                    v2 = to_uint8(x + 1, y + 1, z + 1, block_id, 5, ao[2])
-                    v3 = to_uint8(x + 1, y, z + 1, block_id, 5, ao[3])
+                    v0 = pack_data(x, y, z + 1, block_id, 5, ao[0])
+                    v1 = pack_data(x, y + 1, z + 1, block_id, 5, ao[1])
+                    v2 = pack_data(x + 1, y + 1, z + 1, block_id, 5, ao[2])
+                    v3 = pack_data(x + 1, y, z + 1, block_id, 5, ao[3])
                     index = add_data(vertex_data, index, v0, v2, v1, v0, v3, v2)
 
     return vertex_data[:index]
--- shaders/chunk.vert
+++ shaders/chunk.vert
@@ -1,9 +1,11 @@
 #version 330 core
 
-layout (location = 0) in ivec3 in_position;
-layout (location = 1) in int block_id;
-layout (location = 2) in int face_id;
-layout (location = 3) in int ao_id;
+layout (location = 0) in uint packed_data;
+
+int x, y, z;
+int block_id;
+int face_id;
+int ao_id;
 
 uniform mat4 m_proj;
 uniform mat4 m_view;
@@ -21,10 +23,30 @@ const vec2 uv_coords[4] = vec2[4](vec2(0, 0), vec2(0, 1),  vec2(1, 0), vec2(1, 1
 const int uv_indices[12] = int[12](1, 0, 2, 1, 2, 3, 3, 0, 2, 3, 1, 0);
 const float face_shading[6] = float[6](1.0, 0.5, 0.5, 0.8, 0.5, 0.8);
 
+void unpack(uint packed_data) {
+    // a, b, c, d, e, f, g = x, y, z, voxel_id, face_id, ao_id, flip_id
+    uint b_bit = 6u, c_bit = 6u, d_bit = 8u, e_bit = 3u, f_bit = 2u, g_bit = 1u;
+    uint b_mask = 63u, c_mask = 63u, d_mask = 255u, e_mask = 7u, f_mask = 3u, g_mask = 1u;
+    //
+    uint fg_bit = f_bit + g_bit;
+    uint efg_bit = e_bit + fg_bit;
+    uint defg_bit = d_bit + efg_bit;
+    uint cdefg_bit = c_bit + defg_bit;
+    uint bcdefg_bit = b_bit + cdefg_bit;
+    // unpacking vertex data
+    x = int(packed_data >> bcdefg_bit);
+    y = int((packed_data >> cdefg_bit) & b_mask);
+    z = int((packed_data >> defg_bit) & c_mask);
+    //
+    block_id = int((packed_data >> efg_bit) & d_mask);
+    face_id = int((packed_data >> fg_bit) & e_mask);
+    ao_id = int((packed_data >> g_bit) & f_mask);
+}
+
 void main() {
     f_block_id = block_id;
     f_face_id = face_id;
     uv = uv_coords[uv_indices[gl_VertexID % 6  + (face_id & 1) * 6]];
     shading_color = face_shading[face_id] * ao_values[ao_id];
-    gl_Position = m_proj * m_view * m_model * vec4(in_position, 1.0);
+    gl_Position = m_proj * m_view * m_model * vec4(x, y, z, 1.0);
 }
